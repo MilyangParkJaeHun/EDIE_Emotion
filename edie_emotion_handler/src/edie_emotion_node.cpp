2f50d2fa2585a4a7465272ea536310d2a5ed50b4
MilyangParkJaeHun <jh9277@naver.com>
2021-09-15 09:16:38 +0000

    Add comments

--- edie_emotion_handler/src/edie_emotion_node.cpp
+++ edie_emotion_handler/src/edie_emotion_node.cpp
@@ -1,3 +1,10 @@
+/*
+ * edie_emotion_node.cpp
+ *
+ *  Author: Park Jaehun
+ *  Refactoring: Park Jaehun , 2020.06.29
+ */
+
 #include <ros/ros.h>
 #include <std_msgs/Int8.h>
 #include <std_msgs/Bool.h>
@@ -10,51 +17,59 @@ std_msgs::Int8 battery_low_msgs;
 ros::Publisher display_pub;
 ros::Publisher sound_pub;
 
+/**
+ * @brief Publish emotions state by ros topic.
+ * if the battery is low, not published. 
+ *
+ * @param emotion_state is emotion to express
+ */
+void publish_process(std_msgs::Int8 emotion_state)
+{
+  if(!battery_low) {
+    display_pub.publish(emotion_state);
+
+    if(emotion_state.data > 0)
+    {
+      sound_pub.publish(emotion_state);
+    }
+  }
+}
+
+// Callback of display done state
 void DisplayDoneCallback(const std_msgs::Int8::ConstPtr &msg)
 {
   display_done = msg->data;
-  return;
 }
 
-void publish_process(std_msgs::Int8 emotion_state)
-{
-  if(!battery_low)
-  {
-    display_pub.publish(emotion_state);
-  }
-  if(emotion_state.data > 0)
-  {
-    sound_pub.publish(emotion_state);
-  }
-
-  return;
-}
-
+/**
+ * @brief Callback of emotion state
+ * 
+ * @param msg has emotion state to express
+ */
 void EmotionCallback(const std_msgs::Int8::ConstPtr &msg)
 {
   emotion_state = *msg;
   publish_process(emotion_state);
-
-  return;
 }
 
+/**
+ * @brief Callback of battery state
+ * 
+ * @param msg has a value of true or false
+ * true means battery is low
+ */
 void BatteryLowCallback(const std_msgs::Bool::ConstPtr &msg)
 {
   battery_low = msg->data;
-  
-  return;
 }
 
+// Check if display output is finished.
 bool isDone()
 {
   if(display_done)
-  {
     return true;
-  }
   else
-  {
     return false;
-  }
 }
 
 int main(int argc, char** argv)
@@ -80,9 +95,8 @@ int main(int argc, char** argv)
       display_done = 0;
       emotion_done_pub.publish(done_msgs);
     }
-
-    if(battery_low){
-      battery_low_msgs.data = 6;
+    if(battery_low) {
+      battery_low_msgs.data = 5;
       display_pub.publish(battery_low_msgs);
     }
     loop_rate.sleep();
